d44fd19ba87eec2eeb2487e679eefc9925a4e65d
ibtihelbs <91198460+ibtihelbs@users.noreply.github.com>
2022-06-09 12:12:57 +0200

    commit

--- src/componants/About.js
+++ src/componants/About.js
@@ -4,7 +4,7 @@ import logo from '../images/logo-red.svg';
 import RestaurantFront from '../images/RestaurantFront.svg';
 function About() {
     return (
-      <div className="About px-6">
+      <div className="About px-6" id="About">
        <div className="container mx-auto">
          <img className="w-28 mx-auto py-5" src={logo} alt="logo"/> 
        <p>
@@ -48,7 +48,7 @@ function About() {
   <path strokeLinecap="round" strokeLinejoin="round" d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z" />
 </svg>
     <div className="p-5">
-        <a href="#">
+        <a href="About">
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Gift card</h5>
         </a>
         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
@@ -61,7 +61,7 @@ function About() {
       <path strokeLinecap="round" strokeLinejoin="round" d="M13 16V6a1 1 0 00-1-1H4a1 1 0 00-1 1v10a1 1 0 001 1h1m8-1a1 1 0 01-1 1H9m4-1V8a1 1 0 011-1h2.586a1 1 0 01.707.293l3.414 3.414a1 1 0 01.293.707V16a1 1 0 01-1 1h-1m-6-1a1 1 0 001 1h1M5 17a2 2 0 104 0m-4 0a2 2 0 114 0m6 0a2 2 0 104 0m-4 0a2 2 0 114 0" />
     </svg>
     <div className="p-5">
-        <a href="#">
+        <a href="#About">
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Delivery anywhere</h5>
         </a>
         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
